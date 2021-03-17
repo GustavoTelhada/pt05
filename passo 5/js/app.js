68e9a881bb0be2f5412657b18033b97e861c5516
GustavoTelhada <71329527+GusTelhada@users.noreply.github.com>
2021-03-17 19:54:10 +0000

    update

---
+++ passo 5/js/app.js
@@ -0,0 +1,18 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+
+
+
+for (var i=1; i<28; i++){
+  var aluno = "aluno-";
+  document.getElementById(aluno + i).style.color = getRandomColor();
+}
+
